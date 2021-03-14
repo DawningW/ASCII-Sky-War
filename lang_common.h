a009045ccdf488dd908060687506b983f8ea659d
xaxys <tpnnghd@163.com>
2021-03-15 00:48:47 +0800

    Update API URL

--- lang_common.h
+++ lang_common.h
@@ -61,7 +61,7 @@ static const char* _WCNB[] = {
 };
 
 static const char* _WEBSITE[] = {
-    "http://api.oasis.run/hall",
+    "https://api.oasis.run/hall",
     "https://dawningw.github.io/ASCII-Sky-War/",
     "https://github.com/DawningW/ASCII-Sky-War"
 };
